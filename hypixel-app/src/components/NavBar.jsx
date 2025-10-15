802bec425b42ecd8916042e23369f2fec7c91bb1
IsakPetersson <isak.petersson@elev.ga.ntig.se>
2025-10-15 09:29:19 +0200

    Updated mavbar to also house the current mayor and their perks

--- hypixel-app/src/components/NavBar.jsx
+++ hypixel-app/src/components/NavBar.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 export default function NavBar() {
+  const [mayor, setMayor] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.hypixel.net/v2/resources/skyblock/election")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.mayor) {
+          setMayor(data.mayor);
+        }
+      });
+  }, []);
+
   return (
     <nav className="navbar-vertical">
       <h2>Hypixel Tracker</h2>
@@ -12,6 +25,42 @@ export default function NavBar() {
           <Link to="/login">Login</Link>
         </li>
       </ul>
+      <div style={{ marginTop: "auto", width: "100%" }}>
+        <div
+          style={{
+            color: "#E732AA",
+            fontWeight: "bold",
+            fontSize: "1em",
+          }}
+        >
+          {mayor ? (
+            <>
+              Mayor: {mayor.name}
+              <ul
+                style={{
+                  listStyleType: "dotted",
+                  paddingLeft: "0",
+                  margin: "8px auto 0 auto",
+                  fontWeight: "normal",
+                  fontSize: "0.8em",
+                  color: "#fff",
+                  width: "90%",
+                  textAlign: "center",
+                  display: "block",
+                }}
+              >
+                {mayor.perks.map((perk, idx) => (
+                  <li key={idx} style={{ margin: "2px 0", textAlign: "center" }}>
+                    {perk.name}
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            "Loading mayor..."
+          )}
+        </div>
+      </div>
     </nav>
   );
 }
