bd5ca1c6d5324ef8cf13077899402d650eb0ef54
Eliah Dimmed <153503499+eliahdim@users.noreply.github.com>
2025-10-15 13:24:44 +0200

    hero section & navbar styling

--- hypixel-app/src/pages/Items.jsx
+++ hypixel-app/src/pages/Items.jsx
@@ -3,7 +3,11 @@ import ItemList from "../components/ItemList";
 export default function Items() {
   return (
     <div className="center-container">
-      <h1 className="text-center mb-4 mainTitle">Hypixel Skyblock Items</h1>
+      <div className="hero-section">
+        <h1 className="text-center mb-4 mainTitle">Skyblock Item Tracker</h1>
+          <h2>Discover, search, and track over 1,000 Hypixel Skyblock Items. Find stats, rarities and prices in one comprehensive database.</h2>
+          <button className="browseItems-btn">Browse Items</button>
+      </div>
       <ItemList />
     </div>
   );
